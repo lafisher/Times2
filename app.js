9ecc0bc961f064bc0a842287b479612082838f98
afordfa <anitamford@gmail.com>
2017-02-25 08:33:55 -0600

    found the headline in the api and added jquery script tag to index

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
+$(document).ready(function(){
+// var searchValue = $(".searchTerm").val();
+// var numRecords = ($".numRecords").val();
+// var startYear = $(".startYear").val() + "0101";
+// var endYear = $(".endYear").val() + "1231";
 
-var searchValue = $(".searchTerm").val();
+var searchValue = "penguins";
+var numRecords = 5;
+var startYear = "20110101"
+var endYear = "20161231"
 
 
+
+// $(".clearButton").on("click", function(event) {});
+
+// $(".searchButton").on("click", function(event) {});
 
 
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 url += '?' + $.param({
   'api-key': "5dd8e97f3a674cca9ec9825981c0e780",
-  'q': searchValue
+  'q': searchValue,
+  'begin_date': startYear,
+  'end_date': endYear
 });
+
+
 $.ajax({
   url: url,
   method: 'GET',
 }).done(function(result) {
   console.log(result);
+  console.log(result.response.docs[0].headline.main);
 }).fail(function(err) {
   throw err;
 });
 
 
+});
